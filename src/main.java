2d7184dd5b44fdc70ee3030f2b5e342fdd5876f5
Gabriel Aboy <gabeaboy@gmail.com>
2019-02-12 21:49:44 -0500

    Priority queue sorting by lowest end time

--- src/main.java
+++ src/main.java
@@ -6,32 +6,44 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This could be priority queue instead of array, lets get there now
 		List<Class> classArray = new ArrayList<Class>();
-//		System.out.println("asdasd "+hey.getClassName());
+        Comparator<Class> stringLengthComparator = new Comparator<Class>() {
+            @Override
+            public int compare(Class one, Class two) {
+                return one.getEndTime() - two.getEndTime();
+            }
+        };
+        PriorityQueue<Class> ClassPriorityQueue = new PriorityQueue<>(stringLengthComparator);
+//		PriorityQueue<String> pQueue = new PriorityQueue<String>(); 
+		  
+
 		String fileName = ReadUserInput();
 		try {
-			File file = 
-				      new File(fileName); 
-				    Scanner sc = new Scanner(file); 
+			File file = new File(fileName); 
+			Scanner sc = new Scanner(file); 
 				  
-				    while (sc.hasNextLine()) {
-				    	
-				    	String newLine = sc.nextLine();
-				    	
-				    	String className = newLine.substring(1, 2);
-				    	int startTime = Integer.parseInt(newLine.substring(4, 5));
-				    	int endTime = Integer.parseInt(newLine.substring(7, 8));
-				    	classArray.add(new Class(className,startTime,endTime));
-				    	System.out.println(classArray.get(1).getClassName());
-				    	
-				    }
-				      
-		}catch(IOException e) {
+		    while (sc.hasNextLine()) {
+		    	
+		    	String newLine = sc.nextLine();
+		    	
+		    	String className = newLine.substring(1, 2);
+		    	int startTime = Integer.parseInt(newLine.substring(4, 5));
+		    	int endTime = Integer.parseInt(newLine.substring(7, 8));
+		    	ClassPriorityQueue.add(new Class(className,startTime,endTime));
+		    	
+		    }
+		      
+		}
+		catch(IOException e) {
 			System.out.println("error, file name is incorrect reRun the program");
 			
-		}
+		}	
+		// Start working on priority queue...
 		
-		System.out.println(fileName);
+		  while (!ClassPriorityQueue.isEmpty()) {
+	            System.out.println(ClassPriorityQueue.remove().getClassName());
+	        }
 	}
 	// String function should read input and return string for java open file
     public static String ReadUserInput() {
